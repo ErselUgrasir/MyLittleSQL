70235c269a01d3176bb2c66310923068d68d2061
Ersel Uğraşır <eugrasir@gmail.com>
2016-06-02 23:43:04 +0300

    Create MyLittleSQL.java

---
+++ MyLittleSQL.java
@@ -0,0 +1,15 @@
+package mylittlesql;
+
+import java.util.Scanner;
+
+public class MyLittleSQL {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("#Lütfen müşteri sayısını giriniz: ");
+        Datas d = new Datas(input.nextInt());
+        for (int i = 2; i < d.M; i++) {
+            d.count(i);
+        }
+    }
+}
